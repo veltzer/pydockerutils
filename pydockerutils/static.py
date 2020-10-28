40d5a23e3f116be96b4e01d736d3bb8fa07d249f
Mark Veltzer <mark.veltzer@gmail.com>
2020-10-28 07:17:18 +0200

    new version

--- pydockerutils/static.py
+++ pydockerutils/static.py
@@ -1,4 +1,4 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.4"
+VERSION_STR = "0.0.5"
 DESCRIPTION = "pydockerutils is a set of docker utils"
 APP_NAME = "pydockerutils"
