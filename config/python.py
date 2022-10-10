8092c10f53b3b1e3090deb6d53a5cfcabfbb2152
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-11 00:11:06 +0300

    deps change

--- config/python.py
+++ config/python.py
@@ -2,9 +2,7 @@ console_scripts = [
     "pydockerutils=pydockerutils.main:main",
 ]
 dev_requires = [
-    "pyclassifiers",
     "pypitools",
-    "Sphinx",
 ]
 install_requires = [
     "pytconf",
@@ -13,12 +11,13 @@ install_requires = [
 make_requires = [
     "pymakehelper",
     "pydmt",
+    "pyclassifiers",
+    "Sphinx",
 ]
 test_requires = [
     "pylint",
     "pytest",
     "pytest-cov",
     "flake8",
-    "pymakehelper",
     "mypy",
 ]
