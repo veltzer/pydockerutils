4fa0a35793a3dc94f916f353ddab20749ea67304
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 10:10:05 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,30 +1,23 @@
-""" python depedencies for this project """
-from typing import List
+""" python deps for this project """
 
-
-console_scripts: List[str] = [
+console_scripts: list[str] = [
     "pydockerutils=pydockerutils.main:main",
 ]
-dev_requires: List[str] = [
-    "pypitools",
-    "black",
-]
-config_requires: List[str] = [
+config_requires: list[str] = [
     "pyclassifiers",
 ]
-install_requires: List[str] = [
+install_requires: list[str] = [
     "pytconf",
     "pylogconf",
 ]
-build_requires: List[str] = [
+build_requires: list[str] = [
     "pymakehelper",
     "pydmt",
 ]
-test_requires: List[str] = [
+test_requires: list[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
-    "flake8",
     "mypy",
 ]
 requires = config_requires + install_requires + build_requires + test_requires
