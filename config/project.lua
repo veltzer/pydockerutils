a992ab3d136d281cc69aac4cf3a695a04854f3fb
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:21 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,7 @@
+NAME = "pydockerutils"
+DESCRIPTION_SHORT = "pydockerutils is a set of docker utils"
+KEYWORDS = {
+    "docker",
+    "container",
+    "utils",
+}
--- config/project.py
+++
@@ -1,8 +0,0 @@
-""" project definitions """
-name = "pydockerutils"
-description_short = "pydockerutils is a set of docker utils"
-keywords = [
-    "docker",
-    "container",
-    "utils",
-]
