a4c77b2cd581745084024e61a4e81444d7621948
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-15 17:40:03 +0300

    fix build

--- src/pydockerutils/main.py
+++ src/pydockerutils/main.py
@@ -3,9 +3,9 @@ main file
 """
 
 import pylogconf.core
-from pytconf import register_endpoint, register_main, config_arg_parse_and_launch
+from pytconf import config_arg_parse_and_launch, register_endpoint, register_main
 
-from pydockerutils.static import DESCRIPTION, APP_NAME, VERSION_STR
+from pydockerutils.static import APP_NAME, DESCRIPTION, VERSION_STR
 
 
 @register_endpoint(
